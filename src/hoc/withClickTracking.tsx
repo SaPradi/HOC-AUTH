95e6a00bfa0faf4054da29396945cb72ce7f70aa
SaPradi <a.garces@globant.com>
2024-07-11 11:04:11 -0500

    fix: copy hoc

--- src/hoc/withClickTracking.tsx
+++ src/hoc/withClickTracking.tsx
@@ -2,25 +2,25 @@ import { ComponentType } from "react";
 
 interface WithClickTrackingProps {
     trackingInfo: string;
-    label:string;
+    label: string;
 }
-  
+
 
 const withClickTracking = <P extends WithClickTrackingProps>(WrappedComponent: ComponentType<P>) => {
 
-    return (props:P) => {
+    return (props: P) => {
 
-      const handleClick = () => {
-        console.log('Click tracked:', props.trackingInfo);
-      };
-  
-      return (
-        <div onClick={handleClick}>
-          <WrappedComponent {...props} />
-        </div>
-      );
+        const handleClick = () => {
+            console.log('Click tracked:', props.trackingInfo);
+        };
+
+        return (
+            <div onClick={handleClick}>
+                <WrappedComponent {...props} />
+            </div>
+        );
     };
-  };
+};
 
 
 export default withClickTracking;
--- src/hoc/withPageTracking.tsx
+++ src/hoc/withPageTracking.tsx
@@ -1,29 +1,20 @@
-import React, { ComponentType, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { ComponentType, useEffect } from 'react';
 
-interface WithLoaderProps {
-  loading: boolean;
-  login: () => void;
+interface WithPageTrackingProps {
+  pageName: string;
 }
 
-const withLoaderAuth = <P extends object>(WrappedComponent: ComponentType<P & WithLoaderProps>) => {
-  const WithLoaderAuth: React.FC<P> = (props) => {
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
 
-    const login = () => {
-      setLoading(true);
-      setTimeout(() => {
-        localStorage.setItem('token', 'AUTH_TOKEN_USER');
-        setLoading(false);
-        navigate('/dashboard');
-      }, 1000);
-    };
+const withPageTracking = <P extends object>(WrappedComponent: ComponentType<P>) => {
 
-    return <WrappedComponent {...props as P} loading={loading} login={login} />;
+  return (props: P & WithPageTrackingProps) => {
+
+    useEffect(() => {
+      console.log('Page tracked:', props.pageName);
+
+    }, [props.pageName]);
+
+    return <WrappedComponent {...props} />;
   };
-
-  return WithLoaderAuth;
 };
-
-export default withLoaderAuth;
+export default withPageTracking;
